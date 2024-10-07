18ac0f882202a436274373ecd86ee50226d345ed
Patrick <plmiranda@rdretailgroup.com.ph>
2024-10-07 15:42:16 +0800

    minor tweaks

--- components/google-maps.tsx
+++ components/google-maps.tsx
@@ -14,7 +14,7 @@ interface GoogleMapsSectionProps {
 }
 
 const MemoizedGoogleMapsEmbed = memo(function MemoizedGoogleMapsEmbed({ q }: { q: string }) {
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY || '';
 
   return (
     <GoogleMapsEmbed
@@ -46,7 +46,7 @@ const GoogleMapsSection = memo(function GoogleMapsSection({ locations }: GoogleM
         </div>
 
         <div className="aspect-w-16 aspect-h-9">
-          {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ? (
+          {process.env.GOOGLE_MAPS_API_KEY ? (
             <MemoizedGoogleMapsEmbed q={formattedLocations} />
           ) : (
             <div className="bg-gray-200 flex items-center justify-center h-[450px]">
--- next.config.js
+++ next.config.js
@@ -24,9 +24,6 @@ const nextConfig = {
         },
         ],
       },
-    experimental: {
-      googleMaps: true
-    },
       reactStrictMode: false,
 }
 
